970dcfe414d9502b6fec93318e5c7bab52a8f716
hidr4lisk <federico.furgiuele@gmail.com>
2024-10-10 14:14:05 -0300

    asd

---
+++ js/Practicas/Ejercicios Tipos de Datos/script.js
@@ -0,0 +1,36 @@
+//1. Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+//let nombre = 'Juan'
+//let apellido = 'Wachosky'
+// Imprime: Hola, mi nombre es Juan Wachosky!
+//console.log(`Hola, mi nombre es ${nombre} ${apellido}!`)
+
+
+//2. Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre por pantalla el contenido de la variable.
+//let cadena = '¡Hola Mundo!'
+//console.log(cadena)
+
+
+//3. Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+//let userName = prompt('Escriba su nombre: ')
+//console.log(`Hola ${userName}!`)
+
+
+//4. Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética
+//let operacion = ((3+5)/(2*5))**2
+//console.log(operacion)
+
+
+//5. Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora.
+//Después debe mostrar por pantalla la paga que le corresponde.
+//let horasTrabajadas =  prompt('Ingrese sus horas trabajadas: ')
+//let costePorHora = prompt('Ingrese el coste por hora $: ')
+//let paga = horasTrabajadas * costePorHora
+//console.log(`Su paga es: $${paga}`)
+
+
+//6. Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, y muestre por pantalla la frase Tu índice de masa corporal es <imc> 
+//donde <imc> es el índice de masa corporal calculado redondeado con dos decimales.
+pesoUsuario = Number(prompt('INGRESE su PESO en números: '))
+estaturaUsuario = Number(prompt('INGRESE su ALTURA en números: '))
+p
+console.log()
