75a0eafb9b15c8fe66d70f1393d6336a21897e3d
hidr4lisk <federico.furgiuele@gmail.com>
2024-10-15 23:17:46 -0300

    ultimo1510

---
+++ js/Practicas/Ejercicios Tipos de Datos/clave.js
@@ -0,0 +1,13 @@
+
+//Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+let clave = 0
+while (clave === 0) {
+    clave = prompt('ingrese una clave: ')
+    alert(`La clave es: ${clave}`)
+}
+ingreso = prompt("Ingrese la clave para entrar: ")
+if (ingreso.toLowerCase() === clave.toLowerCase()){
+    alert("Contraseña correcta")
+} else {
+    alert("Contraseña incorrecta")
+}
---
+++ js/Practicas/Ejercicios Tipos de Datos/divisor.js
@@ -0,0 +1,7 @@
+//Escribir un programa que pida al usuario dos números y muestre por pantalla su división. 
+//Si el divisor es cero el programa debe mostrar un error.
+
+let numero_uno = Number(prompt("Ingrese el primer número: "))
+let numero_dos = Number(prompt("Ingrese el segundo número: "))
+alert(`El primer número: ${numero_uno} dividido el segundo: ${numero_dos}`)
+
