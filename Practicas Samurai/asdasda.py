8277ebfc5d52804da54786d5b21f96ba61251f94
hidr4lisk <federico.furgiuele@gmail.com>
2024-10-18 11:16:30 -0300

    sd

---
+++ Practicas Samurai/asdasda.py
@@ -0,0 +1,8 @@
+"""
+Dado el siguiente diccionario:
+paises = {
+    "Canadá": {"capital": "Ottawa", "habitantes": 1015400},
+    "Alemania": {"capital": "Berlín", "habitantes": 3644826},
+}
+Eliminar el contenido de la clave 'Alemania' y mostrar el diccionario resultante
+"""
---
+++ Practicas Samurai/texto.py
@@ -0,0 +1,19 @@
+"""
+Transforma el texto en:
+Gordon lanzó su curva...
+- Strawberry ha fallado por un pie! -gritó Joe Castiglione.
+- Dos pies -le corrigió Troop.
+- Strawberry menea la cabeza como disgustado... -agrega el comentarista.
+"""
+"""
+INFO_2 = "gordon lanzó su curva&strawberry ha fallado por un pie! -gritó Joe Castiglione&dos pies -le corrigió Troop&strawberry menea la cabeza como disgustado… -agrega el comentarista&Mike Jhonson se acerca a alentar a su compañero"
+"""
+original = "gordon lanzó su curva&strawberry ha fallado por un pie! -gritó Joe Castiglione&dos pies -le corrigió Troop&strawberry menea la cabeza como disgustado… -agrega el comentarista&Mike Jhonson se acerca a alentar a su compañero"
+
+lista_frases = original.lower().split("&")
+nombres = ["gordon","strawberry","joe","castiglione","troop","mike","jhonson"]
+
+for frase in lista_frases:
+    for i in nombres:
+        frase.replace(i, i.capitalize())
+    print(frase)
