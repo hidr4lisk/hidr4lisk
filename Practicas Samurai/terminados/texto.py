b3964806aad6d8004cc93ce631edbb38780fba49
hidr4lisk <federico.furgiuele@gmail.com>
2024-10-21 11:59:31 -0300

    asdf

--- Practicas Samurai/terminados/texto.py
+++ Practicas Samurai/terminados/texto.py
@@ -5,9 +5,7 @@ Gordon lanzó su curva...
 - Dos pies -le corrigió Troop.
 - Strawberry menea la cabeza como disgustado... -agrega el comentarista.
 """
-"""
-INFO_2 = "gordon lanzó su curva&strawberry ha fallado por un pie! -gritó Joe Castiglione&dos pies -le corrigió Troop&strawberry menea la cabeza como disgustado… -agrega el comentarista&Mike Jhonson se acerca a alentar a su compañero"
-"""
+
 original = "gordon lanzó su curva...&strawberry ha fallado por un pie! -gritó Joe Castiglione&dos pies -le corrigió Troop&strawberry menea la cabeza como disgustado… -agrega el comentarista&Mike Jhonson se acerca a alentar a su compañero"
 
 lista_frases = original.lower().split("&")
